117a8cadd40b3b8276ebacad0322612b573ad80c
Jake Levy <jake.r.levy@gmail.com>
2017-08-02 16:19:06 -0400

    Initial Commit

---
+++ CauseFeedProto2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CauseFeedProto2
+//
+//  Created by Jacob Levy on 8/2/17.
+//  Copyright © 2017 Jacob Levy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
